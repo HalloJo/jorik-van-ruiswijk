a20cce93b65c56d9c1641691cf30e9f0be47285c
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-07 15:34:17 +0100

    Create Social section

--- components/Me/Me.jsx
+++ components/Me/Me.jsx
@@ -1,14 +1,30 @@
 import styles from '../Me/Me.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faInstagram, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
-import Link from 'next/link'
+import { skills } from '../../data/Skills'
 
 const Me = () => {
     return (
         <section className={styles.me}>
              <h2 className={styles.me__heading}>Me</h2>
+             <div className={styles.me__synopsis}>
+                <p className={styles.me__text}>Jorik van Ruiswijk. 31 years of age. Based in the Netherlands. Self-taught front-end developer. Graduated graphic designer and illustrator. Loves his kids, family and football. Helps people with their design or development problems.</p>
+             </div>
+             <div className={styles.me__skills}>
+                <h3 className={styles.me__subHeading}>Skills</h3>
+                <div className={styles.me__skillsGrid}>
+                    { skills.map((skill)=> {
+                        return (
+                            <div className={styles.me__skillWrapper} key={skill.skill}>
+                                <FontAwesomeIcon className={styles.me__skillIcon} icon={skill.icon} />
+                                <p className={styles.me__skillTitle}>{skill.skill}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+             </div>
              <div className={styles.me__socials}>
-                <h3>Find me here</h3>
+                <h3 className={styles.me__subHeading}>Find me here</h3>
                 <div className={styles.me__icons}>
                     <a className={styles.me__socialIcon} href="https://www.linkedin.com/in/jorikvanruiswijk/" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon className={styles.me__fontAwesomeIcon} icon={faLinkedin} />
@@ -20,7 +36,6 @@ const Me = () => {
                         <FontAwesomeIcon className={styles.me__fontAwesomeIcon} icon={faGithub} />
                     </a>
                 </div>
-
              </div>
         </section>
     )
---
+++ data/Skills.ts
@@ -0,0 +1,58 @@
+export const skills = [
+    {
+        icon: ['fa','atom'],
+        skill: "React.js"
+    },
+    {
+        icon: ['fa','forward'],
+        skill: "Next.js"
+    },
+    {
+        icon: ['fa','rocket'],
+        skill: "Astro"
+    },
+    {
+        icon: ['fa','superscript'],
+        skill: "TypeScript"
+    },
+    {
+        icon: ['fa','hand-peace'],
+        skill: "Vue.js"
+    },
+    {
+        icon: ['fa','spider'],
+        skill: "Webdesign"
+    },
+    {
+        icon: ['fa','code'],
+        skill: "HTML5"
+    },
+    {
+        icon: ['fa','pencil'],
+        skill: "Illustration"
+    },
+    {
+        icon: ['fa','arrows-turn-right'],
+        skill: "GSAP"
+    },
+    {
+        icon: ['fa','palette'],
+        skill: "Branding"
+    },
+    {
+        icon: ['fa','a'],
+        skill: "Adobe"
+    },
+    {
+        icon: ['fa','shirt'],
+        skill: "CSS3"
+    },
+    {
+        icon: ['fa','rocket'],
+        skill: "Figma"
+    },
+    {
+        icon: ['fa','scroll'],
+        skill: "Print"
+    },
+];
