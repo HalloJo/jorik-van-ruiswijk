a7172d4b9a9003aff21a164f8ae22693fa2db2fe
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-09 14:22:04 +0100

    Add submit type to button and set up Contact component with validation

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,11 +1,12 @@
+import { type } from 'os';
 import styles from '../Button/Button.module.scss'
 
 
-const Button = ({label, href, style}:{label: string; href: string; style: string;}) => {
+const Button = ({label, href, style, type}:{label: string; href: string; style: string; type: "button" | "submit" | "reset"}) => {
 
     return (
             <a href={href} className={styles.link}>
-                <button className={`${styles.button} ${styles[style]}`}>
+                <button type={type} className={`${styles.button} ${styles[style]}`}>
                     {label}
                 </button>
             </a>
--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,12 +1,33 @@
 import styles from '../Contact/Contact.module.scss'
 import Heading from '../Heading/Heading'
+import { useForm } from 'react-hook-form'
+import Button from '../Button/Button'
 
 const Contact = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
     return (
         <section id='contact' className={styles.contact}>
             <Heading heading="Contact" exception={"beige"} />
-            <form>
-
+            <form onSubmit={handleSubmit()} className={styles.contact__form}>
+                <div className={styles.contact__inputWrapper}>
+                    <label className={styles.contact__inputLabel} htmlFor="">Full name*</label>
+                    <input {...register("fullName", { required: true })} className={styles.contact__input} type="text" placeholder='Type your full name here..' />
+                    {errors.fullName && <span className={styles.contact__errorMessage}>👆 Your full name is required!</span>}
+                </div>
+                <div className={styles.contact__inputWrapper}>
+                    <label className={styles.contact__inputLabel} htmlFor="">E-mail*</label>
+                    <input {...register("email", { required: true, pattern: { value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/, message: "Email is not valid."
+                    } })} className={styles.contact__input} type="email"  placeholder='example@provider.com' />
+                    {errors.email && <span className={styles.contact__errorMessage}>👆 A real email address is required!</span>}
+                </div>
+                <div className={styles.contact__inputWrapper}>
+                    <label className={styles.contact__inputLabel} htmlFor="">Your message*</label>
+                    <textarea {...register("message", { required: true })} className={styles.contact__input} type="text"  placeholder='Your message..' />
+                    {errors.message && <span className={styles.contact__errorMessage}>👆 A message is required!</span>}
+                </div>
+                <Button type='submit' label='Submit' />
             </form>
         </section>
     )
