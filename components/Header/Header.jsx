4d6d001fd6ddfb3c4b9314cffa6c1e4c4cc39613
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-09 14:48:04 +0100

    Style desktop design Header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -10,14 +10,16 @@ const Header = () => {
         <header className={styles.header}>
             <div className={styles.header__content}>
                 <HeroArt />
+            </div>
+            <div className={styles.header__callToActions}>
                 <div className={styles.header__contentText}>
                     <h1 className={styles.header__name}>Jorik</h1>
                     <p className={styles.header__subTitle}>Design & Development</p>
                 </div>
-            </div>
-            <div className={styles.header__callToActions}>
-                <Button label={"See work"} href={"#work"} style={"orange"} />
-                <Button label={"See me"} href={"#me"} style={"green"} />
+                <div className={styles.header__buttons}>
+                    <Button label={"See work"} href={"#work"} style={"orange"} />
+                    <Button label={"See me"} href={"#me"} style={"green"} />
+                </div>
             </div>
       </header>
     )
