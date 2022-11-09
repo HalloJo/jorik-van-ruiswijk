7bd65672b95f2c50e60fefb60040759e4444ea12
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-09 11:21:46 +0100

    Move images to different folder, add color variables, add work data

--- data/WorkCarouselData.ts
+++ data/WorkCarouselData.ts
@@ -1,25 +1,43 @@
 export const carouselData = [
     {
         href: "https://buitenkanz.com/",
-        src: "/assets/buitenkanz.png",
+        src: "/assets/work/buitenkanz.png",
         alt: "Buitenkanz logo",
         label: "Website / identity"
     },
     {
         href: "https://master--react-bookclub.netlify.app/",
-        src: "/assets/bookclub.png",
+        src: "/assets/work/bookclub.png",
         alt: "Bookclub logo",
         label: "React / Api"
     },
     {
+        href: "https://www.behance.net/gallery/65567759/midocean",
+        src: "/assets/work/midocean.png",
+        alt: "midocean logo",
+        label: "Branding / Webdesign / Illustration"
+    },
+    {
         href: "https://react-jiffy.netlify.app/",
-        src: "/assets/jiffy.png",
+        src: "/assets/work/jiffy.png",
         alt: "Jiffy logo",
         label: "Application / React / Api"
     },
     {
+        href: "https://1-project-fifty-festival.superhi.com/",
+        src: "/assets/work/fifty.png",
+        alt: "Fifty logo",
+        label: "Website / HTML5"
+    },
+    {
+        href: "https://3-type-checker.superhi.com/",
+        src: "/assets/work/typechecker.png",
+        alt: "Type Checker logo",
+        label: "Application / HTML5"
+    },
+    {
         href: "https://4-good-times.superhi.com/",
-        src: "/assets/goodtimes.png",
+        src: "/assets/work/goodtimes.png",
         alt: "Good Times logo",
         label: "Website / HTML5"
     },
