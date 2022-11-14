020de7f6bf9caf832acfdb483a8b7294c2fda2f7
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-14 11:47:40 +0100

    Add animation to Heading component

--- components/Heading/Heading.jsx
+++ components/Heading/Heading.jsx
@@ -1,9 +1,32 @@
-import styles from '../Heading/Heading.module.scss'
+import styles from "../Heading/Heading.module.scss";
+import { motion as m } from "framer-motion";
 
-const Heading = ({heading, exception}) => {
-    return (
-        <h2 className={`${styles.heading} ${styles[exception]}`}>{heading}</h2>
-    )
-}
+const Heading = ({ heading, exception }) => {
+  const headingAnimation = {
+    hidden: {
+      opacity: 0,
+      translateY: 40,
+    },
+    visible: {
+      opacity: 1,
+      translateY: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
 
-export default Heading
+  return (
+    <m.h2
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false }}
+      variants={headingAnimation}
+      className={`${styles.heading} ${styles[exception]}`}
+    >
+      {heading}
+    </m.h2>
+  );
+};
+
+export default Heading;
