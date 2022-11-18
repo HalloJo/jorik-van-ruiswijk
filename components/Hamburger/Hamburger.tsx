70e350db6be4275f8cce6820b51ab672f0f4cbd6
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-18 13:57:23 +0100

    Add overflow: hidden to HTML when HamburgerMenu is open

--- components/Hamburger/Hamburger.tsx
+++ components/Hamburger/Hamburger.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import styles from "../Hamburger/Hamburger.module.scss";
 
 type HamburgerProps = {
@@ -6,6 +7,13 @@ type HamburgerProps = {
 };
 
 const Hamburger = ({ open, setOpen }: HamburgerProps) => {
+  useEffect(() => {
+    const HTML = document.querySelector("html");
+    if (HTML) {
+      HTML.style.overflow = open ? "hidden" : "auto";
+    }
+  }, [open]);
+
   return (
     <div className={styles.hamburger} onClick={() => setOpen(!open)}>
       <span className={styles.hamburger__line}></span>
