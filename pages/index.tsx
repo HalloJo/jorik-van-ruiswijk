49065a9775fedc024c89f473e37ce278819bcfe3
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-18 13:57:09 +0100

    Remove footer from Index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
-import Head from 'next/head'
-import Navigation from '../components/Navigation/Navigation'
-import Header from '../components/Header/Header'
-import styles from '../styles/Home.module.scss'
-import Work from '../components/Work/Work'
-import Me from '../components/Me/Me'
-import Contact from '../components/Contact/Contact'
+import Head from "next/head";
+import Navigation from "../components/Navigation/Navigation";
+import Header from "../components/Header/Header";
+import styles from "../styles/Home.module.scss";
+import Work from "../components/Work/Work";
+import Me from "../components/Me/Me";
+import Contact from "../components/Contact/Contact";
 
 const Home = () => {
-
   return (
     <div className={styles.container}>
       <Head>
         <title>Jorik - Design & Development</title>
-        <meta name="description" content="Need help with design or development? Jorik can help you with identity and websites. Just what you need." />
+        <meta
+          name="description"
+          content="Need help with design or development? Jorik can help you with identity and websites. Just what you need."
+        />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
@@ -27,11 +29,8 @@ const Home = () => {
         <Me />
         <Contact />
       </main>
-
-      <footer className={styles.footer}>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
