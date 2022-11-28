54090400568b3ef4ce9180c8b9c3ead68daa2c45
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-28 13:45:34 +0100

    Add resize event listener for full height section and hamburger menu

--- components/CustomCursor/CustomCursor.tsx
+++ components/CustomCursor/CustomCursor.tsx
@@ -45,10 +45,10 @@ const CustomCursor = () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-    // window.addEventListener("resize", () => {
-    //   let vh = window.innerHeight * 0.01;
-    //   document.documentElement.style.setProperty("--vh", `${vh}px`);
-    // });
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
 
     return () => {
       window.removeEventListener("mousemove", mouseMove);
