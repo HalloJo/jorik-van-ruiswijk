9f16808c08ce90af8d8fdd0c066ba3db82a97ba7
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-28 13:26:09 +0100

    Create CustomCursor component, tweak animation

---
+++ components/CustomCursor/CustomCursor.tsx
@@ -0,0 +1,67 @@
+import { motion as m, Variants } from "framer-motion";
+import { useState, useEffect } from "react";
+import styles from "../CustomCursor/CustomCursor.module.scss";
+
+const CustomCursor = () => {
+  type MousePositionProps = {
+    x: number;
+    y: number;
+  };
+
+  const [mousePosition, setMousePosition] = useState<MousePositionProps>({
+    x: 0,
+    y: 0,
+  });
+  const [mouseDown, setMouseDown] = useState<boolean>(false);
+
+  const mouseMove = (event) => {
+    setMousePosition({
+      x: event.clientX - 7.5,
+      y: event.clientY - 7.5,
+    });
+  };
+
+  const mouseVariant: Variants = {
+    default: {
+      x: mousePosition.x,
+      y: mousePosition.y,
+      transition: {
+        type: "spring",
+        damping: 30,
+        stiffness: 600,
+      },
+    },
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", mouseMove);
+    window.addEventListener("mousedown", () => {
+      setMouseDown(true);
+    });
+    window.addEventListener("mouseup", () => {
+      setMouseDown(false);
+    });
+
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    // window.addEventListener("resize", () => {
+    //   let vh = window.innerHeight * 0.01;
+    //   document.documentElement.style.setProperty("--vh", `${vh}px`);
+    // });
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  return (
+    <m.div
+      variants={mouseVariant}
+      animate="default"
+      className={`${styles.cursor} ${mouseDown && styles.down}`}
+    />
+  );
+};
+
+export default CustomCursor;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,64 +4,10 @@ import Header from "../components/Header/Header";
 import styles from "../styles/Home.module.scss";
 import Work from "../components/Work/Work";
 import Me from "../components/Me/Me";
-import Wave from "../components/Wave/Wave";
 import Contact from "../components/Contact/Contact";
-import { useEffect, useState } from "react";
-import { motion as m, Variants } from "framer-motion";
+import CustomCursor from "../components/CustomCursor/CustomCursor";
 
 const Home = () => {
-  type MousePositionProps = {
-    x: number;
-    y: number;
-  };
-
-  const [mousePosition, setMousePosition] = useState<MousePositionProps>({
-    x: 0,
-    y: 0,
-  });
-  const [mouseDown, setMouseDown] = useState<boolean>(false);
-
-  const mouseMove = (event) => {
-    setMousePosition({
-      x: event.clientX - 7.5,
-      y: event.clientY - 7.5,
-    });
-  };
-
-  const mouseVariant: Variants = {
-    default: {
-      x: mousePosition.x,
-      y: mousePosition.y,
-      transition: {
-        type: "spring",
-        damping: 30,
-        stiffness: 400,
-      },
-    },
-  };
-
-  useEffect(() => {
-    window.addEventListener("mousemove", mouseMove);
-    window.addEventListener("mousedown", () => {
-      setMouseDown(true);
-    });
-    window.addEventListener("mouseup", () => {
-      setMouseDown(false);
-    });
-
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-    // window.addEventListener("resize", () => {
-    //   let vh = window.innerHeight * 0.01;
-    //   document.documentElement.style.setProperty("--vh", `${vh}px`);
-    // });
-
-    return () => {
-      window.removeEventListener("mousemove", mouseMove);
-    };
-  }, []);
-
   return (
     <div className={styles.container}>
       <Head>
@@ -108,12 +54,7 @@ const Home = () => {
         <Me />
         <Contact />
       </main>
-
-      <m.div
-        variants={mouseVariant}
-        animate="default"
-        className={`${styles.cursor} ${mouseDown && styles.down}`}
-      />
+      <CustomCursor />
     </div>
   );
 };
