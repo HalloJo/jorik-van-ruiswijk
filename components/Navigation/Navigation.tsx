e3aac05c4b8f58f59d6d2a79b999f234ca160ff6
HalloJo <jorikvanruiswijk@gmail.com>
2023-02-20 14:24:10 +0100

    Center component in Page component

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -2,10 +2,11 @@ import Logo from "../Logo/Logo";
 import NavigationList from "../NavigationList/NavigationList";
 import { navigation } from "../../data/NavigationData";
 import Hamburger from "../Hamburger/Hamburger";
-import styles from "../Navigation/Navigation.module.scss";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 import { useEffect, useState } from "react";
 import { motion as m, Variants } from "framer-motion";
+import Container from "../Container/Container";
+import styles from "../Navigation/Navigation.module.scss";
 
 const Navigation = () => {
   const [open, setOpen] = useState<boolean>(false);
@@ -41,12 +42,14 @@ const Navigation = () => {
       variants={navigationAnimation}
       className={`${styles.navigation} ${scrolled && styles.scrolled}`}
     >
-      <div className={styles.navigation__logoWrapper}>
-        <Logo />
-      </div>
-      <Hamburger open={open} setOpen={setOpen} />
-      <NavigationList navigation={navigation} />
-      <HamburgerMenu navigation={navigation} open={open} setOpen={setOpen} />
+      <Container>
+        <div className={styles.navigation__logoWrapper}>
+          <Logo />
+        </div>
+        <NavigationList navigation={navigation} />
+        {/* <Hamburger open={open} setOpen={setOpen} /> */}
+        {/* <HamburgerMenu navigation={navigation} open={open} setOpen={setOpen} /> */}
+      </Container>
     </m.nav>
   );
 };
