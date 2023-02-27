a7a20ca3a9487759079c051593e06902bd3195d9
HalloJo <jorikvanruiswijk@gmail.com>
2023-02-27 12:58:51 +0100

    Fix width of Footer component in PageWrapper

--- components/PageWrapper/PageWrapper.tsx
+++ components/PageWrapper/PageWrapper.tsx
@@ -1,25 +1,30 @@
-import { ReactNode } from "react"
-import Footer from "../Footer/Footer"
-import Navigation from "../Navigation/Navigation"
-import Page from "../Page/Page"
+import { ReactNode } from "react";
+import Footer from "../Footer/Footer";
+import * as Links from "../../data/Links";
+import Navigation from "../Navigation/Navigation";
+import Page from "../Page/Page";
 
 interface PageProps {
-    children: ReactNode,
-    className?: string,
+  children: ReactNode;
+  className?: string;
 }
 
-const PageWrapper = ({children, className}: PageProps) => {
-    return (
-        <Page>
-            <Page.Header>
-                <Navigation/>
-            </Page.Header>
-            {children}
-            <Page.Footer>
-                <Footer/>
-            </Page.Footer>
-        </Page>
-    )
-}
+const PageWrapper = ({ children, className }: PageProps) => {
+  return (
+    <Page>
+      <Page.Header>
+        <Navigation navigation={Links.Navigation} social={Links.Social} />
+      </Page.Header>
+      {children}
+      <Page.Footer>
+        <Footer
+          social={Links.Social}
+          telephone={Links.Telephone}
+          email={Links.Email}
+        />
+      </Page.Footer>
+    </Page>
+  );
+};
 
-export default PageWrapper
+export default PageWrapper;
