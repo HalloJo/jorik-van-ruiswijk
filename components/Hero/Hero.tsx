28ee0542c39380025b91cbac9905ce27314b0d83
HalloJo <jorikvanruiswijk@gmail.com>
2023-03-06 16:01:35 +0100

    Refactor Hero to animate some elements

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,19 +3,40 @@ import { motion as m, Variants } from "framer-motion";
 import Container from "../Container/Container";
 import Row from "../Row/Row";
 import Column from "../Column/Column";
-import HeroIcon from "../HeroIcon/HeroIcon";
-import Button from "../Button/Button";
 
 const Header = () => {
+  const container: Variants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+        staggerDirection: 1,
+        // delayChildren: 1,
+        ease: "easeIn",
+      },
+    },
+  };
+
   const item: Variants = {
     hidden: { opacity: 0, y: 10 },
     visible: {
       opacity: 1,
       y: 0,
       transition: {
-        staggerChildren: 0.3,
-        duration: 0.5,
-        delay: 0.5,
+        duration: 0.3,
+      },
+    },
+  };
+
+  const arrows: Variants = {
+    hidden: { opacity: 0, y: -10 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+        delay: 1,
       },
     },
   };
@@ -23,38 +44,43 @@ const Header = () => {
   return (
     <header className={styles.hero}>
       <Container className={styles.hero__container}>
-        <m.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          variants={item}
-        >
-          <Row>
-            <Column className={styles.hero__text} width="12 md:10">
-              <h1 className={styles.hero__title}>Jorik van Ruiswijk.</h1>
-              <h2 className={styles.hero__subtitle}>Designer & Developer.</h2>
-              <p className={styles.hero__summary}>
-                Let me help you with identity, logo design, websites,
-                applications, illustrations, icon design and more. Check out my
-                work and let me know what you need.
-              </p>
-              <div className={styles.hero__arrows}>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-                <p className={styles.hero__arrow}>&darr;</p>
-              </div>
-            </Column>
-          </Row>
-        </m.div>
+        <Row>
+          <Column
+            as={m.div}
+            variants={container}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            className={styles.hero__text}
+            width="12 md:10"
+          >
+            <m.h1 variants={item} className={styles.hero__title}>
+              Jorik van Ruiswijk.
+            </m.h1>
+            <m.h2 variants={item} className={styles.hero__subtitle}>
+              Designer & Developer.
+            </m.h2>
+            <m.p variants={item} className={styles.hero__summary}>
+              Let me help you with identity, logo design, websites,
+              applications, illustrations, icon design and more. Check out my
+              work and let me know what you need.
+            </m.p>
+            <m.div variants={arrows} className={styles.hero__arrows}>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+              <p className={styles.hero__arrow}>&darr;</p>
+            </m.div>
+          </Column>
+        </Row>
       </Container>
     </header>
   );
