2bcaa46b0cb4a2b91811ddbae2e8ac93616568ed
HalloJo <jorikvanruiswijk@gmail.com>
2023-03-09 14:09:31 +0100

    Add Typography component and mixins to Work section

--- components/Work/Work.tsx
+++ components/Work/Work.tsx
@@ -5,16 +5,23 @@ import Heading from "../Heading/Heading";
 import { motion as m, Variants } from "framer-motion";
 import Container from "../Container/Container";
 import * as Animation from "../../utils/animations";
+import Typography from "../Typography/Typography";
 
 const Work = () => {
   return (
     <section id="work" className={styles.work}>
       <Container className={styles.work__container}>
-        <Heading className={styles.work__heading} heading="Work" />
-        <m.p {...Animation.inViewChild} className={styles.work__synopsis}>
+        <Typography variant="h2" className={styles.work__heading}>
+          Work
+        </Typography>
+        <Typography
+          variant="body"
+          {...Animation.inViewChild}
+          className={styles.work__synopsis}
+        >
           Below is a small grasp of projects. Projects that I have drawn,
           designed, collaborated on and developed.
-        </m.p>
+        </Typography>
         <div className={styles.work__carouselContainer}>
           <m.ul
             {...Animation.inViewContainer}
