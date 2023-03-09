50543fe9786062a798f2d04546c845a2e710dc10
HalloJo <jorikvanruiswijk@gmail.com>
2023-03-09 16:56:18 +0100

    Animate Hamburgermenu and tweak Workcard, add small animation to Me section

--- components/HamburgerMenu/HamburgerMenu.tsx
+++ components/HamburgerMenu/HamburgerMenu.tsx
@@ -1,6 +1,8 @@
 import { LinkProps, Navigation, Social } from "../../data/Links";
 import Container from "../Container/Container";
 import styles from "../HamburgerMenu/HamburgerMenu.module.scss";
+import { motion as m, Variants } from "framer-motion";
+import * as Animation from "../../utils/animations";
 import Marquee from "../Marquee/Marquee";
 import SocialLinks from "../SocialLinks/SocialLinks";
 import cn from "clsx";
@@ -17,23 +19,27 @@ const HamburgerMenu = ({ navigation, open, setOpen }: HamburgerMenuProps) => {
   return (
     <nav className={cn(styles.hamburgerMenu, open && styles.open)}>
       <Container className={styles.hamburgerMenu__container}>
-        <ul className={styles.hamburgerMenu__list}>
+        <m.ul
+          className={styles.hamburgerMenu__list}
+          {...Animation.inViewContainer}
+        >
           {navigation.map((item) => {
             const { label, href } = item;
             return (
-              <a
+              <m.a
                 className={styles.hamburgerMenu__link}
                 key={label}
                 href={href}
                 onClick={() => setOpen(!open)}
+                {...Animation.inViewChild}
               >
                 <li className={styles.hamburgerMenu__label}>
                   <Typography variant="h2">{label}</Typography>
                 </li>
-              </a>
+              </m.a>
             );
           })}
-        </ul>
+        </m.ul>
         <SocialLinks
           isStacked
           socials={Social}
