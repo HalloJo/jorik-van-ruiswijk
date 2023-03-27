0f0eab047e055f6a0d41795aaaff2edcdd329051
HalloJo <jorikvanruiswijk@gmail.com>
2023-03-27 10:50:14 +0200

    Add Framer scroll based on Y progress

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import styles from "../Hero/Hero.module.scss";
-import { motion as m, Variants } from "framer-motion";
+import { motion as m, useScroll, useTransform, Variants } from "framer-motion";
 import * as Animation from "../../utils/animations";
 import * as Content from "../../data/Content";
 import Container from "../Container/Container";
@@ -7,11 +7,20 @@ import Row from "../Row/Row";
 import Column from "../Column/Column";
 import Typography from "../Typography/Typography";
 import HeroArt from "../HeroArt/HeroArt";
+import { useRef } from "react";
 
 const Header = () => {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["center center", "end center"],
+  });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "10%"]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+
   return (
     <header className={styles.hero}>
-      <Container className={styles.hero__container}>
+      <Container className={styles.hero__container} ref={heroRef}>
         <Row>
           <Column
             as={m.div}
@@ -22,6 +31,7 @@ const Header = () => {
             <Typography
               variant="h1"
               as={m.div}
+              style={{ x, opacity }}
               className={styles.hero__title}
               {...Animation.inViewChild}
             >
@@ -30,6 +40,7 @@ const Header = () => {
             <Typography
               variant="h1"
               as={m.div}
+              style={{ x, opacity }}
               {...Animation.inViewChild}
               className={styles.hero__subtitle}
             >
@@ -38,12 +49,17 @@ const Header = () => {
             <Typography
               variant="body"
               as={m.div}
+              style={{ x, opacity }}
               {...Animation.inViewChild}
               className={styles.hero__summary}
             >
               {Content.Hero.intro}
             </Typography>
-            <m.div {...Animation.arrows} className={styles.hero__arrows}>
+            <m.div
+              {...Animation.arrows}
+              style={{ x, opacity }}
+              className={styles.hero__arrows}
+            >
               <Typography variant="body" className={styles.hero__arrow}>
                 &darr;
               </Typography>
